31bcfe3025f4f5560f446305c2bab913090fca66
felipendelicia <delicia4581@gmail.com>
2022-05-30 18:33:50 -0300

    style: landing styles

--- client/src/components/Landing/Landing.tsx
+++ client/src/components/Landing/Landing.tsx
@@ -1,15 +1,48 @@
-import React from 'react'
+import React from "react";
 
-import config from '../../config'
-import "./Landing.css"
+import config from "../../config";
+import "./Landing.css";
+import "./LandingContent.css";
+
+const landingContainerStyle = {
+  backgroundImage:`url(${config.imgURL.landingImage})`,
+  backgroundPosition: "center",
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+};
+
+const LandingContent = () => {
+  return (
+    <div className="landing-content-container">
+      <div className="landing-text-container">
+        <h1>Barberok</h1>
+        <p>Sacá un turno para cortarte en la mejor barberia.</p>
+      </div>
+      <div className="landing-btns-container">
+        <div className="landing-btn-take-turn">
+          <button>Sacar turno</button>
+        </div>
+        <div className="landing-btns-contact">
+          <a href={config.externURL.clientInstagram} target="_blank" rel="noreferrer">
+            <img alt="barber instagram" src={config.imgURL.instagram}/>
+          </a>
+          <a href={config.externURL.clientWhatsapp} target="_blank" rel="noreferrer">
+            <img alt="barber whatsapp" src={config.imgURL.whatsapp}/>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const Landing = () => {
   return (
-    <div className="landing-container">
-      <div className='landing-container-main'>
+    <div className="landing-container-main" style={landingContainerStyle}>
+      <div className="landing-container">
+        <LandingContent />.
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
--- client/src/config.ts
+++ client/src/config.ts
@@ -1,12 +1,14 @@
 const config = {
     imgURL:{
-        instagram:"https://cdn2.iconfinder.com/data/icons/social-icons-33/128/Instagram-256.png",
-        whatsapp:"https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/375_Whatsapp_logo-256.png",
+        instagram:"https://cdn1.iconfinder.com/data/icons/social-circle-3/32/instagram_circle-256.png",
+        whatsapp:"https://cdn4.iconfinder.com/data/icons/social-media-flat-7/64/Social-media_Whatsapp-256.png",
         favicon:"",
-        landingImage:"./assets/img/landing-img.jpg"
+        landingImage:"http://192.168.0.10:3000/assets/img/landing-img.jpg"
     },
     externURL:{
-        programmerLinkedIn:"https://www.linkedin.com/in/felipe-nahuel-delicia-b2487119a/"
+        programmerLinkedIn:"https://www.linkedin.com/in/felipe-nahuel-delicia-b2487119a/",
+        clientInstagram:"https://www.instagram.com/_barberok/",
+        clientWhatsapp:"https://wa.me/+5491163062103"
     }
 }
 
